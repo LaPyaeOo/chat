eaac4a0b971ae4968e3b17b8ddcfea705e24b427
LaPyae <lapyaeoo@ayainnovation.com>
2024-07-21 18:26:27 +0630

    add: add signup screen

--- lib/presentation/page/signup_page.dart
+++ lib/presentation/page/signup_page.dart
@@ -1,10 +1,14 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-import '../widget/custom_button.dart';
-import '../widget/custom_text_input.dart';
+import '../../data/app_color.dart';
+import '../../data/app_image.dart';
+import '../../data/config/app_size.dart';
+import '../widget/outline_text_field.dart';
+import '../widget/rounded_button.dart';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -14,131 +18,281 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   final _auth = FirebaseAuth.instance;
   String? email;
+
   // String username;
   String? password;
   String? name;
   bool signingup = false;
+
   @override
   Widget build(BuildContext context) {
     return ModalProgressHUD(
       inAsyncCall: signingup,
       child: Scaffold(
+        backgroundColor: AppColor.backgroundRegular,
+        appBar: AppBar(
+          backgroundColor: AppColor.backgroundRegular,
+        ),
         body: SingleChildScrollView(
-          child: Container(
+          child: SizedBox(
             height: MediaQuery.of(context).size.height,
             // margin: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.2),
-            child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  const Text('Create your account'),
-                  const Text(
-                      'To begin using the chat GPT, please create an account with your email address.'),
-                  Hero(
-                    tag: 'heroicon',
-                    child: Icon(
-                      Icons.textsms,
-                      size: 120,
-                      color: Colors.deepPurple[900],
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.02,
-                  ),
                   Text(
-                    'Chat',
+                    'Create your account',
                     style: TextStyle(
-                        color: Colors.deepPurple[900],
-                        fontFamily: 'Poppins',
-                        fontSize: 26,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
-                  ),
-                  CustomTextInput(
-                    hintText: 'Name',
-                    leading: Icons.text_format,
-                    obscure: false,
-                    userTyped: (value) {
-                      name = value;
-                    },
-                  ),
-                  CustomTextInput(
-                    hintText: 'Email',
-                    leading: Icons.mail,
-                    keyboard: TextInputType.emailAddress,
-                    obscure: false,
-                    userTyped: (value) {
-                      email = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 0,
-                  ),
-                  CustomTextInput(
-                    hintText: 'Password',
-                    leading: Icons.lock,
-                    keyboard: TextInputType.visiblePassword,
-                    obscure: true,
-                    userTyped: (value) {
-                      password = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Hero(
-                    tag: 'signupbutton',
-                    child: CustomButton(
-                      onpress: () async {
-                        if (name != null && password != null && email != null) {
-                          setState(() {
-                            signingup = true;
-                          });
-                          try {
-                            final newUser =
-                                await _auth.createUserWithEmailAndPassword(
-                                    email: email!, password: password!);
-                            // UserUpdateInfo info = UserUpdateInfo();
-                            await FirebaseAuth.instance.currentUser!.updateProfile(displayName: name);
-                            // info.displayName = name;
-                            // await newUser.user!.updateProfile(info);
-
-                            Navigator.pushNamed(context, '/chat');
-                                                    } catch (e) {
-                            print('WHATISE --- $e');
-                            setState(() {
-                              signingup = false;
-                            });
-                            Fluttertoast.showToast(msg: 'Signup Failed',gravity: ToastGravity.BOTTOM,);
-                          }
-                        } else {
-                          Fluttertoast.showToast(msg: 'Signup Failed',gravity: ToastGravity.BOTTOM,);
-                        }
-                      },
-                      text: 'sign up',
-                      accentColor: Colors.white,
-                      mainColor: Colors.deepPurple,
+                      color: AppColor.textHighlightPrimary,
+                      fontSize: 26,
+                      fontFamily: 'Ranade',
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
-                  const SizedBox(
-                    height: 5,
+                  tinyVerticalSpace,
+                  tinyVerticalSpace,
+                  Text(
+                    'To begin using the chat GPT, please create an account with your email address.',
+                    style: TextStyle(
+                      color: AppColor.foregroundSubdued,
+                      fontSize: 14,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacementNamed(context, '/login');
-                      },
-                      child: const Text(
-                        'or log in instead',
+                  massiveVerticalSpace,
+                  OutlinedTextField(
+                    labelText: 'Name',
+                    keyboardType: TextInputType.text,
+                    userTyped: (val) {
+                      name = val;
+                    },
+                  ),
+                  veryLargeVerticalSpace,
+                  OutlinedTextField(
+                    labelText: 'Email',
+                    keyboardType: TextInputType.emailAddress,
+                    userTyped: (val) {
+                      email = val;
+                    },
+                  ),
+                  veryLargeVerticalSpace,
+                  OutlinedTextField(
+                    labelText: 'Password',
+                    obscureText: false,
+                    keyboardType: TextInputType.text,
+                    userTyped: (val) {
+                      password = val;
+                    },
+                  ),
+                  tinyVerticalSpace,
+                  tinyVerticalSpace,
+                  Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      Checkbox(
+                        value: true,
+                        onChanged: (value) {},
+                        visualDensity: VisualDensity.comfortable,
+                        activeColor: Colors.indigo,
+                      ),
+                      Text(
+                        'By continuing you agree to the chat GPT ',
                         style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: AppColor.foregroundSecondary,
+                        ),
+                      ),
+                      Text(
+                        'Term of Service',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          decoration: TextDecoration.underline,
+                          color: AppColor.foregroundSecondary,
+                        ),
+                      ),
+                      Text(
+                        ' and ',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: AppColor.foregroundSecondary,
+                        ),
+                      ),
+                      Text(
+                        'Privacy Policy',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          decoration: TextDecoration.underline,
+                          color: AppColor.foregroundSecondary,
+                        ),
+                      ),
+                    ],
+                  ),
+                  veryLargeVerticalSpace,
+                  RoundedButton(
+                    onPressed: () async {
+                      if (name != null && password != null && email != null) {
+                        setState(() {
+                          signingup = true;
+                        });
+                        try {
+                          final newUser =
+                              await _auth.createUserWithEmailAndPassword(
+                                  email: email!, password: password!);
+                          // UserUpdateInfo info = UserUpdateInfo();
+                          await FirebaseAuth.instance.currentUser!
+                              .updateProfile(displayName: name);
+                          // info.displayName = name;
+                          // await newUser.user!.updateProfile(info);
+
+                          Navigator.pushNamed(context, '/chat');
+                        } catch (e) {
+                          print('WHATISE --- $e');
+                          setState(() {
+                            signingup = false;
+                          });
+                          Fluttertoast.showToast(
+                            msg: 'Signup Failed',
+                            gravity: ToastGravity.BOTTOM,
+                          );
+                        }
+                      } else {
+                        Fluttertoast.showToast(
+                          msg: 'Signup Failed',
+                          gravity: ToastGravity.BOTTOM,
+                        );
+                      }
+                    },
+                    radius: 15,
+                    child: const Text(
+                      'Continue',
+                    ),
+                  ),
+                  mediumVerticalSpace,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already have an account? ',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: AppColor.foregroundSecondary,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacementNamed(context, '/login');
+                        },
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
                             fontFamily: 'Poppins',
-                            fontSize: 12,
-                            color: Colors.deepPurple),
-                      ),),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.1,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.foregroundSecondary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  largeVerticalSpace,
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Expanded(
+                        child: Divider(
+                          endIndent: 8,
+                        ),
+                      ),
+                      Text(
+                        'OR',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: AppColor.foregroundSubdued,
+                        ),
+                      ),
+                      const Expanded(
+                        child: Divider(
+                          indent: 8,
+                        ),
+                      ),
+                    ],
+                  ),
+                  largeVerticalSpace,
+                  RoundedButton(
+                    onPressed: () async {},
+                    radius: 15,
+                    backgroundColor: Colors.transparent,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          AppImage.googleLogoIcon,
+                          height: 20,
+                        ),
+                        tinyHorizontalSpace,
+                        tinyHorizontalSpace,
+                        Text(
+                          'Continue with Google',
+                          style: TextStyle(
+                            color: AppColor.textHighlightPrimary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  smallVerticalSpace,
+                  RoundedButton(
+                    onPressed: () async {},
+                    radius: 15,
+                    backgroundColor: const Color(0xff1877F2),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.facebook,
+                          size: 20,
+                        ),
+                        tinyHorizontalSpace,
+                        tinyHorizontalSpace,
+                        Text('Continue with Facebook'),
+                      ],
+                    ),
+                  ),
+                  smallVerticalSpace,
+                  RoundedButton(
+                    backgroundColor: Colors.black,
+                    onPressed: () async {},
+                    radius: 15,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      textBaseline: TextBaseline.ideographic,
+                      children: [
+                        Icon(
+                          Icons.apple,
+                          size: 20,
+                        ),
+                        tinyHorizontalSpace,
+                        tinyHorizontalSpace,
+                        Text('Continue with Facebook'),
+                      ],
+                    ),
                   ),
                 ],
               ),
